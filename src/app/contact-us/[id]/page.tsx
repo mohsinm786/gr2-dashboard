2ae6f1412a4b32879b92e6946fac52e97f881f81
Mohsin M. Mujawar <mohsinmmujawar91561@gmail.com>
2024-11-17 21:58:11 +0530

    Commit

--- src/app/contact-us/[id]/page.tsx
+++ src/app/contact-us/[id]/page.tsx
@@ -1,37 +1,40 @@
-// "use-client"
-// import React from 'react';
-// import { useRouter } from 'next/router';
-// import { useEffect, useState } from 'react';
+"use client";
+import React from 'react';
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-// export default async function DetailedPage({ queryparams }: { queryparams: { id: string } }) {
-//   const route = useRouter();
-//   const { id } = route.query;
-//   const [packageData, setPackageData] = useState(null);
+export default function DetailedPage() {
+    // const router = useParams();
+    const { id } = useParams();
+    const [packageData, setPackageData] = useState(null);
 
-//   useEffect(() = > {
-//     if(id)
-//   })
-//   if (!packageData) {
-//     return (
-//       <div className="p-6 bg-white shadow-md rounded-lg">
-//         <h1 className="text-2xl font-bold mb-4">Package not found</h1>
-//         <p>We could not find the package you are looking for.</p>
-//       </div>
-//     );
-//   }
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`http://localhost:5241/Contact?id=${id}`, {
+                method: 'DELETE'
+            }
+            );
+            const result = await response.json();
+            setPackageData(result);
+        }
+        fetchData();
+    }, [id])
 
-//   return (
-//     <div className="p-6 bg-white shadow-md rounded-lg">
-//       <h1 className="text-2xl font-bold mb-4">{packageData.name}</h1>
-//       <p>
-//         <strong>Name:</strong> ${packageData.name}
-//       </p>
-//       <p>
-//         <strong>Email:</strong> {packageData.email}
-//       </p>
-//       <p>
-//         <strong>Message:</strong> {packageData.message}
-//       </p>
-//     </div>
-//   );
-// }
+    if (!packageData || packageData.length === 0) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <div className="p-6 bg-white shadow-md rounded-lg">
+            <p>
+                <strong>Name:</strong> {packageData?.name}
+            </p>
+            <p>
+                <strong>Email:</strong> {packageData?.email}
+            </p>
+            <p>
+                <strong>Message:</strong> {packageData?.message}
+            </p>
+        </div>
+    );
+}
